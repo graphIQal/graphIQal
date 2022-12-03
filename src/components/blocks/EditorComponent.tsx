846f6f5396274fa758ec8638ff8920046c9f56eb
Isabelle Ilyia <isabelle.ilyia@gmail.com>
2022-12-03 13:40:19 -0500

    starting floating menu

--- src/components/blocks/EditorComponent.tsx
+++ src/components/blocks/EditorComponent.tsx
@@ -9,189 +9,204 @@ import { ReactEditor } from 'slate-react';
 import BlockMenu from './BlockMenu/BlockMenu';
 import { Leaf, DefaultElement, CodeElement } from './Elements/Elements';
 import EditorCommands from './EditorCommands';
+import HoveringToolbar from './Elements/HoveringToolbar';
+import { getAutomaticTypeDirectiveNames } from 'typescript';
 
 type CustomElement = {
-	type: 'paragraph' | 'code';
-	children: (CustomText | CustomElement)[];
+  type: 'paragraph' | 'code';
+  children: (CustomText | CustomElement)[];
 };
 
 type CustomText = {
-	text: string;
-	bold?: boolean;
-	italics?: boolean;
-	text_type: 'text' | 'h1' | 'h2' | 'h3';
+  text: string;
+  bold?: boolean;
+  italics?: boolean;
+  text_type: 'text' | 'h1' | 'h2' | 'h3';
 };
 
 declare module 'slate' {
-	interface CustomTypes {
-		Editor: BaseEditor & ReactEditor;
-		Element: CustomElement;
-		Text: CustomText;
-	}
+  interface CustomTypes {
+    Editor: BaseEditor & ReactEditor;
+    Element: CustomElement;
+    Text: CustomText;
+  }
 }
 
 const initialValue: CustomElement[] = [
-	{
-		type: 'paragraph',
-		children: [{ text: 'TITLE PAGE', text_type: 'h1' }],
-	},
-	{
-		type: 'paragraph',
-		children: [
-			{ text: 'A line of text in a paragraph.', text_type: 'text' },
-		],
-	},
+  {
+    type: 'paragraph',
+    children: [{ text: 'TITLE PAGE', text_type: 'h1' }],
+  },
+  {
+    type: 'paragraph',
+    children: [{ text: 'A line of text in a paragraph.', text_type: 'text' }],
+  },
 ];
 
 const EditorComponent: React.FC = () => {
-	const [editor] = useState(() => withReact(createEditor()));
+  const [editor] = useState(() => withReact(createEditor()));
 
-	// ELEMENTS
-	// Define a React component renderer for our code blocks.
+  // ELEMENTS
+  // Define a React component renderer for our code blocks.
 
-	// Define a rendering function based on the element passed to `props`. We use
-	// `useCallback` here to memoize the function for subsequent renders.
-	const renderElement = useCallback((props: any) => {
-		switch (props.element.type) {
-			case 'code':
-				return <CodeElement {...props} />;
-			default:
-				return <DefaultElement {...props} />;
-		}
-	}, []);
+  // Define a rendering function based on the element passed to `props`. We use
+  // `useCallback` here to memoize the function for subsequent renders.
+  const renderElement = useCallback((props: any) => {
+    switch (props.element.type) {
+      case 'code':
+        return <CodeElement {...props} />;
+      default:
+        return <DefaultElement {...props} />;
+    }
+  }, []);
 
-	const renderLeaf = useCallback((props: any) => {
-		return <Leaf {...props} />;
-	}, []);
+  const renderLeaf = useCallback((props: any) => {
+    return <Leaf {...props} />;
+  }, []);
 
-	const showMenu = () => {
-		// <div style={}></div>;
-	};
-	return (
-		<>
-			<BlockMenu
-				items={[
-					{
-						title: 'text',
-						onPress: () => {
-							Transforms.setNodes(
-								editor,
-								{ text_type: 'text' },
-								// Apply it to text nodes, and split the text node up if the
-								// selection is overlapping only part of it.
-								{
-									match: (n) => Text.isText(n),
-								}
-							);
-						},
-					},
-					{
-						title: 'Header 1',
-						onPress: () => {
-							Transforms.setNodes(
-								editor,
-								{ text_type: 'h1' },
-								// Apply it to text nodes, and split the text node up if the
-								// selection is overlapping only part of it.
-								{
-									match: (n) => Text.isText(n),
-								}
-							);
-						},
-					},
-					{
-						title: 'Header 2',
-						onPress: () => {
-							Transforms.setNodes(
-								editor,
-								{ text_type: 'h2' },
-								// Apply it to text nodes, and split the text node up if the
-								// selection is overlapping only part of it.
-								{
-									match: (n) => Text.isText(n),
-								}
-							);
-						},
-					},
-					{
-						title: 'Header 3',
-						onPress: () => {
-							Transforms.setNodes(
-								editor,
-								{ text_type: 'h3' },
-								// Apply it to text nodes, and split the text node up if the
-								// selection is overlapping only part of it.
-								{
-									match: (n) => Text.isText(n),
-								}
-							);
-						},
-					},
-					{
-						title: 'bold',
-						onPress: () => {
-							Transforms.setNodes(
-								editor,
-								{ bold: true },
-								// Apply it to text nodes, and split the text node up if the
-								// selection is overlapping only part of it.
-								{
-									match: (n) => Text.isText(n),
-									split: true,
-								}
-							);
-						},
-					},
-					{
-						title: 'italics',
-						onPress: () => {
-							const match = Editor.nodes(editor, {
-								match: (n) =>
-									Editor.isBlock(editor, n) &&
-									n.type === 'paragraph',
-							});
-						},
-					},
-					{
-						title: 'test',
-						onPress: () => {},
-					},
-				]}
-			/>
-			<Slate editor={editor} value={initialValue}>
-				<Editable
-					renderElement={renderElement}
-					renderLeaf={renderLeaf}
-					onKeyDown={(event) => {
-						if (!event.ctrlKey && !event.metaKey) {
-							// if (event.key === 'Slash') {
-							// 	showMenu();
-							// }
-							return;
-						}
-						console.log(event);
+  const [opacity, setOpacity] = useState<'0%' | '100%'>('0%');
+  const showMenu = () => {
+    setOpacity('100%');
+    // editor.insertNode()
+  };
 
-						switch (event.key) {
-							// When "`" is pressed, keep our existing code block logic.
-							case 'k': {
-								console.log('```');
-								event.preventDefault();
-								EditorCommands.toggleCodeBlock(editor);
-								break;
-							}
+  const getItems = () => {
+    return [
+      {
+        title: 'text',
+        onPress: () => {
+          Transforms.setNodes(
+            editor,
+            { text_type: 'text' },
+            // Apply it to text nodes, and split the text node up if the
+            // selection is overlapping only part of it.
+            {
+              match: (n) => Text.isText(n),
+            }
+          );
+        },
+      },
+      {
+        title: 'Header 1',
+        onPress: () => {
+          Transforms.setNodes(
+            editor,
+            { text_type: 'h1' },
+            // Apply it to text nodes, and split the text node up if the
+            // selection is overlapping only part of it.
+            {
+              match: (n) => Text.isText(n),
+            }
+          );
+        },
+      },
+      {
+        title: 'Header 2',
+        onPress: () => {
+          Transforms.setNodes(
+            editor,
+            { text_type: 'h2' },
+            // Apply it to text nodes, and split the text node up if the
+            // selection is overlapping only part of it.
+            {
+              match: (n) => Text.isText(n),
+            }
+          );
+        },
+      },
+      {
+        title: 'Header 3',
+        onPress: () => {
+          Transforms.setNodes(
+            editor,
+            { text_type: 'h3' },
+            // Apply it to text nodes, and split the text node up if the
+            // selection is overlapping only part of it.
+            {
+              match: (n) => Text.isText(n),
+            }
+          );
+        },
+      },
+      {
+        title: 'bold',
+        onPress: () => {
+          Transforms.setNodes(
+            editor,
+            { bold: true },
+            // Apply it to text nodes, and split the text node up if the
+            // selection is overlapping only part of it.
+            {
+              match: (n) => Text.isText(n),
+              split: true,
+            }
+          );
+        },
+      },
+      {
+        title: 'italics',
+        onPress: () => {
+          const match = Editor.nodes(editor, {
+            match: (n) => Editor.isBlock(editor, n) && n.type === 'paragraph',
+          });
+        },
+      },
+      {
+        title: 'test',
+        onPress: () => {},
+      },
+    ];
+  };
+  return (
+    <>
+      <Slate editor={editor} value={initialValue}>
+        <HoveringToolbar opacity={opacity} />
 
-							// When "B" is pressed, bold the text in the selection.
-							case 'b': {
-								event.preventDefault();
-								EditorCommands.toggleBoldMark(editor);
-								break;
-							}
-						}
-					}}
-				/>
-			</Slate>
-		</>
-	);
+        <BlockMenu items={getItems()} />
+        <Editable
+          renderElement={renderElement}
+          renderLeaf={renderLeaf}
+          onKeyDown={(event) => {
+            if (event.key === '&') {
+              event.preventDefault();
+              editor.insertText('and');
+            }
+            if (event.key === '/') {
+              showMenu();
+            } else {
+              setOpacity('0%');
+            }
+
+            // if (Editor.before(editor, editor.selection?.anchor))
+            if (!event.ctrlKey && !event.metaKey) {
+              // if (event.key === 'Slash') {
+              // 	showMenu();
+              // }
+              return;
+            }
+            console.log(event);
+
+            switch (event.key) {
+              // When "`" is pressed, keep our existing code block logic.
+              case 'k': {
+                console.log('```');
+                event.preventDefault();
+                EditorCommands.toggleCodeBlock(editor);
+                break;
+              }
+
+              // When "B" is pressed, bold the text in the selection.
+              case 'b': {
+                event.preventDefault();
+                EditorCommands.toggleBoldMark(editor);
+                break;
+              }
+            }
+          }}
+        />
+      </Slate>
+    </>
+  );
 };
 
 export default EditorComponent;
---
+++ src/components/blocks/Elements/HoveringToolbar.tsx
@@ -0,0 +1,172 @@
+import React, { useMemo, useRef, useEffect, useState } from 'react';
+import { Slate, Editable, withReact, useSlate, useFocused } from 'slate-react';
+import {
+  Editor,
+  Transforms,
+  Text,
+  createEditor,
+  Descendant,
+  Range,
+} from 'slate';
+import { format } from 'path';
+import BlockMenu from '../BlockMenu/BlockMenu';
+
+type HoveringToolbarProps = {
+  opacity: '0%' | '100%';
+};
+const HoveringToolbar: React.FC<HoveringToolbarProps> = ({ opacity }) => {
+  // const ref = useRef<HTMLDivElement | null>();
+  const editor = useSlate();
+  const inFocus = useFocused();
+
+  // useEffect(() => {
+  //   const el = ref.current;
+  //   const { selection } = editor;
+
+  //   if (!el) {
+  //     return;
+  //   }
+
+  //   if (
+  //     !selection ||
+  //     !inFocus ||
+  //     Range.isCollapsed(selection) ||
+  //     Editor.string(editor, selection) === ''
+  //   ) {
+  //     // setOpacity('0%');
+  //     return;
+  //   }
+
+  //   const domSelection = window.getSelection();
+  //   if (domSelection) {
+  //     const domRange = domSelection.getRangeAt(0);
+  //     const rect = domRange.getBoundingClientRect();
+  //     el.style.opacity = '1';
+  //     el.style.top = `${rect.top + window.pageYOffset - el.offsetHeight}px`;
+  //     el.style.left = `${
+  //       rect.left + window.pageXOffset - el.offsetWidth / 2 + rect.width / 2
+  //     }px`;
+  //   }
+  // });
+
+  //   const getPixelPosition = function(position, onScreen) {
+  //     if (!position)
+  //         position = this.session.selection.getCursor();
+  //     var pos =
+  //     var cursorLeft = this.$padding + pos.column * this.config.characterWidth;
+  //     var cursorTop = (pos.row - (onScreen ? this.config.firstRowScreen : 0)) *
+  //         this.config.lineHeight;
+
+  //     return {left : cursorLeft, top : cursorTop};
+  // };
+  return (
+    <div
+      style={{
+        opacity: opacity,
+        display: 'inline-block',
+        padding: '8px 7px 6px',
+        position: 'absolute',
+        zIndex: 1,
+
+        // top: '-10000px',
+        // left: '-10000px',
+        marginTop: '6px',
+        backgroundColor: '#00000',
+        borderRadius: '4px',
+
+        marginLeft: '200px',
+      }}
+    >
+      <BlockMenu
+        items={[
+          {
+            title: 'text',
+            onPress: () => {
+              Transforms.setNodes(
+                editor,
+                { text_type: 'text' },
+                // Apply it to text nodes, and split the text node up if the
+                // selection is overlapping only part of it.
+                {
+                  match: (n) => Text.isText(n),
+                }
+              );
+            },
+          },
+          {
+            title: 'Header 1',
+            onPress: () => {
+              Transforms.setNodes(
+                editor,
+                { text_type: 'h1' },
+                // Apply it to text nodes, and split the text node up if the
+                // selection is overlapping only part of it.
+                {
+                  match: (n) => Text.isText(n),
+                }
+              );
+            },
+          },
+          {
+            title: 'Header 2',
+            onPress: () => {
+              Transforms.setNodes(
+                editor,
+                { text_type: 'h2' },
+                // Apply it to text nodes, and split the text node up if the
+                // selection is overlapping only part of it.
+                {
+                  match: (n) => Text.isText(n),
+                }
+              );
+            },
+          },
+          {
+            title: 'Header 3',
+            onPress: () => {
+              Transforms.setNodes(
+                editor,
+                { text_type: 'h3' },
+                // Apply it to text nodes, and split the text node up if the
+                // selection is overlapping only part of it.
+                {
+                  match: (n) => Text.isText(n),
+                }
+              );
+            },
+          },
+          {
+            title: 'bold',
+            onPress: () => {
+              Transforms.setNodes(
+                editor,
+                { bold: true },
+                // Apply it to text nodes, and split the text node up if the
+                // selection is overlapping only part of it.
+                {
+                  match: (n) => Text.isText(n),
+                  split: true,
+                }
+              );
+            },
+          },
+          {
+            title: 'italics',
+            onPress: () => {
+              const match = Editor.nodes(editor, {
+                match: (n) =>
+                  Editor.isBlock(editor, n) && n.type === 'paragraph',
+              });
+            },
+          },
+          {
+            title: 'test',
+            onPress: () => {},
+          },
+        ]}
+      />
+    </div>
+  );
+};
+
+export default HoveringToolbar;

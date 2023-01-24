7508b4c866d4b92278a508f5586b76f9dd497828
Isabelle Ilyia <isabelle.ilyia@gmail.com>
2023-01-24 08:40:23 -0500

    starting codegen

--- codegen.ts
+++ codegen.ts
@@ -1,17 +1,13 @@
 import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
-  overwrite: true,
   schema: 'http://localhost:4000',
-  documents: 'src/**/*.tsx',
+  documents: ['src/**/*.tsx'],
   ignoreNoDocuments: true,
   generates: {
     './src/gql/': {
       preset: 'client',
       plugins: [],
-    },
-    './graphql.schema.json': {
-      plugins: ['introspection'],
     },
   },
 };
--- src/helpers/backend/userHelpers.tsx
+++ src/helpers/backend/userHelpers.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { gql, useQuery } from 'urql';
 import { Query } from './dbAccessObj';
+import { graphql } from '../../gql/gql';
 
 //Method to get the current user's information from their ID (statically passed in for now, because we're dealing with one user)
 export const GetCurrentUser = (id: string, go: boolean) => {
-  const getCurrentUserQuery = gql`
+  const getCurrentUserQuery = `
     query ($id: ID) {
       users(where: { id: $id }) {
         id
@@ -14,6 +15,15 @@ export const GetCurrentUser = (id: string, go: boolean) => {
       }
     }
   `;
+  console.log(
+    'querying' +
+      useQuery({
+        query: getCurrentUserQuery,
+        variables: { id },
+      })
+  );
+
+  // console.log(data)
 
   interface UserQuery {
     users: {

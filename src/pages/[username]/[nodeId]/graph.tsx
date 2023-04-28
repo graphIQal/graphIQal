7e96a3846829e3336434cfcd1b16d0324cc1eede
Isabelle Ilyia <isabelle.ilyia@gmail.com>
2023-04-28 13:57:11 -0400

    moving files

--- src/pages/[username]/[nodeId]/graph.tsx
+++ src/pages/[username]/[nodeId]/graph.tsx
@@ -3,131 +3,17 @@
  * Creates and sets all the global props that go into Context wrappers
  */
 
-import React, { useEffect, useRef, useState } from 'react';
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
+import React, { useEffect, useState } from 'react';
 
-import DrawingContext from '../../../packages/graph/context/GraphDrawingContext';
-import { LineRefs } from '../../../packages/graph/graphTypes';
-import GraphViewContext from '../../../packages/graph/context/GraphViewContext';
-import { BoxDragLayer } from '../../../packages/graph/helpers/BoxDragLayer';
-import { handleDrawingHotkey } from '../../../packages/graph/hooks/drawingHooks';
-import { GraphContainer } from '../../../packages/graph/components/GraphContainer';
-import {
-  getAllNodes,
-  getLines,
-  getNodesToDisplay,
-  getNodesToDisplayGraph,
-} from '../../../helpers/backend/getHelpers';
-
-const Graph: React.FC<{ window: Window; document: Document }> = ({
-  window,
-  document,
-}) => {
-  if (!document || !window) return <div></div>;
-  //Data of all nodes
-  const [allNodes, setAllNodes] = useState(getAllNodes());
-  //Graph in view of one node
-  const [nodeInView, setNodeInView] = useState('homenode');
-  //Data of nodes to display (comes from Connection information between each node and the node in view)
-  const [nodesDisplayed, setNodesDisplayed] = useState(
-    getNodesToDisplay(nodeInView, allNodes)
-  );
-
-  //Visual attributes of nodes to display
-  const [nodesVisual, setNodesVisual] = useState(
-    getNodesToDisplayGraph(nodeInView, allNodes, window, document)
-  );
-
+import Graph from '../../../packages/graph/components/graph';
+const Graph2: React.FC = () => {
+  const [windowVar, setWindow] = useState<Window>();
+  const [documentVar, setDocument] = useState<Document>();
   useEffect(() => {
-    setNodesDisplayed(getNodesToDisplay(nodeInView, allNodes));
-    setNodesVisual(
-      getNodesToDisplayGraph(nodeInView, allNodes, window, document)
-    );
-  }, [nodeInView]);
-
-  //Line data
-  const [lines, setLines] = useState<LineRefs[]>(getLines());
-
-  //Drawing states
-  const containerRef = useRef(null);
-  const [drawingMode, setDrawingMode] = useState(true);
-  const [isDrawing, setIsDrawing] = useState<boolean>(false);
-
-  //Drawing line data
-  const startNode = useRef<string>('');
-  const endNode = useRef<string>('');
-
-  //Line functions for detecting arrows
-  let isPointInCanvasFuncs = useRef<any>({});
-  let numPointsInTriangleFuncs = useRef<any>({});
-
-  //Modal to show node details and connection details
-  const [modalNode, setModalNode] = useState('');
-
-  const [showModalConnection, setShowModalConnection] = useState(false);
-  const [currConnection, setCurrConnection] = useState(lines[0]);
-
-  // Hot key for undo/redo
-  useEffect(() => {
-    const listenerFunc = (evt: any) => {
-      evt.stopImmediatePropagation();
-      if (evt.code === 'KeyZ' && (evt.ctrlKey || evt.metaKey) && evt.shiftKey) {
-        // redo();
-      } else if (evt.code === 'KeyZ' && (evt.ctrlKey || evt.metaKey)) {
-        // undo();
-      }
-    };
-    document.addEventListener('keydown', (event) => listenerFunc(event));
-    return document.removeEventListener('keydown', (event) =>
-      listenerFunc(event)
-    );
-  }, []);
-
-  return (
-    <DndProvider backend={HTML5Backend}>
-      <div
-        onKeyDown={(event) =>
-          handleDrawingHotkey(event, drawingMode, setDrawingMode)
-        }
-        tabIndex={-1}
-        ref={containerRef}
-      >
-        <DrawingContext.Provider
-          value={{
-            startNode: startNode,
-            endNode: endNode,
-            isPointInCanvasFuncs: isPointInCanvasFuncs,
-            numPointsInTriangleFuncs: numPointsInTriangleFuncs,
-            drawingMode: drawingMode,
-            setDrawingMode: setDrawingMode,
-            isDrawing: isDrawing,
-            setIsDrawing: setIsDrawing,
-          }}
-        >
-          <GraphViewContext.Provider
-            value={{
-              lines,
-              setLines,
-              setNodeInView: setNodeInView,
-              nodesDisplayed: nodesDisplayed,
-              setNodesDisplayed: setNodesDisplayed,
-              nodesVisual: nodesVisual,
-              setNodesVisual: setNodesVisual,
-              modalNode: modalNode,
-              setModalNode: setModalNode,
-              nodeInView: nodeInView,
-              allNodes: allNodes,
-              setAllNodes: setAllNodes,
-            }}
-          >
-            <GraphContainer window={window} document={document} />
-            <BoxDragLayer parentRef={containerRef} />
-          </GraphViewContext.Provider>
-        </DrawingContext.Provider>
-      </div>
-    </DndProvider>
-  );
+    setWindow(window);
+    setDocument(document);
+  });
+  return <Graph window={windowVar} document={documentVar} />;
 };
 
-export default Graph;
+export default Graph2;

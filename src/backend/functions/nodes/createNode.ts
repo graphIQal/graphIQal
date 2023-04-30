053a372b71185e3427fe726629adcc14efd415dd
JesseLiii <jesseli751@gmail.com>
2023-04-30 12:47:52 -0400

    theoretically working add graph node

--- src/backend/functions/nodes/createNode.ts
+++ src/backend/functions/nodes/createNode.ts
@@ -2,17 +2,27 @@ type createNodeInput = {
 	userId: string;
 	currentNodeId: string;
 	nodeTitle: string;
+	graphViewId: string;
+	x_cell: number;
+	y_cell: number;
+	x_size: number;
+	y_size: number;
 };
 
 export const createNode = async ({
 	userId,
 	currentNodeId,
 	nodeTitle,
+	graphViewId,
+	x_cell,
+	y_cell,
+	x_size,
+	y_size,
 }: createNodeInput) => {
 	console.log('login attempted ');
 
 	const res = await fetch(
-		`/api/${userId}/${currentNodeId}/create?nodeTitle=${nodeTitle}`
+		`/api/${userId}/${currentNodeId}/create?nodeTitle=${nodeTitle}&x_cell=${x_cell}&y_cell=${y_cell}&x_size=${x_size}&y_size=${y_size}`
 	)
 		.then((res) => {
 			console.log('res ', res);

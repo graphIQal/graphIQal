1775311ed6a3e9e8a6f3635495fa2bde6d07c8ab
JesseLiii <jesseli751@gmail.com>
2023-05-05 17:38:23 -0400

    save graph working

--- src/backend/functions/graph/saveGraphView.ts
+++ src/backend/functions/graph/saveGraphView.ts
@@ -25,25 +25,29 @@ export const saveGraphView = async ({
 }: saveGraphViewInput) => {
 	console.log('Save graph view');
 
-	const body = jsonToCypher_graphView({ nodeData, graphViewData });
+	const body = jsonToCypher_graphView({
+		nodeData,
+		graphViewData,
+		graphViewId,
+	});
 	// const body = JSON.stringify({ ...nodeData, ...graphViewData });
 	console.log(body);
 
-	// const res = await fetch(
-	// 	`/api/${username}/${nodeId}/graph/${graphViewId}/save`,
-	// 	{
-	// 		method: 'POST',
-	// 		body: body,
-	// 	}
-	// )
-	// 	.then((res) => {
-	// 		console.log('res ', res);
-	// 		return res.json();
-	// 	})
-	// 	.then((json) => {
-	// 		console.log('json: ', json);
-	// 		return json;
-	// 	});
+	const res = await fetch(
+		`/api/${username}/${nodeId}/graph/${graphViewId}/save`,
+		{
+			method: 'POST',
+			body: body,
+		}
+	)
+		.then((res) => {
+			console.log('res ', res);
+			return res.json();
+		})
+		.then((json) => {
+			console.log('json: ', json);
+			return json;
+		});
 
-	// return res;
+	return res;
 };
--- src/pages/api/[username]/[nodeId]/graph/[graphViewId]/save.ts
+++ src/pages/api/[username]/[nodeId]/graph/[graphViewId]/save.ts
@@ -7,25 +7,25 @@ export default async function handler(
 	req: NextApiRequest,
 	res: NextApiResponse
 ) {
-	const params = req.query;
+	const body = req.body;
 
 	// Add Hash for password}
-	const cypher = `
-	MATCH (o:Node {id: $currentNodeId})
-	MATCH (g:GRAPH_VIEW {id: $graphViewId})
+	// const cypher = `
+	// MATCH (o:Node {id: $currentNodeId})
+	// MATCH (g:GRAPH_VIEW {id: $graphViewId})
 
-	MERGE (o)-[r:IN]->(n:Node {title: $nodeTitle, id: randomUuid()})<-[r:IN {
-		x_size: $x_size,
-		y_size: $y_size,
-		x_cell: $x_cell,
-		y_cell: $y_cell,
-		collapsed: "true
-	}]-(g)
+	// MERGE (o)-[r:IN]->(n:Node {title: $nodeTitle, id: randomUuid()})<-[r:IN {
+	// 	x_size: $x_size,
+	// 	y_size: $y_size,
+	// 	x_cell: $x_cell,
+	// 	y_cell: $y_cell,
+	// 	collapsed: "true
+	// }]-(g)
 
-	RETURN n, g
-	`;
+	// RETURN n, g
+	// `;
 
-	// const result: any = await write(cypher as string, params);
+	const result: any = await write(body as string);
 
-	res.status(200).json({ ...params });
+	res.status(200).json({ ...result });
 }

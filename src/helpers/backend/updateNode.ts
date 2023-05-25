1cd7b01fa798ca790ec7c580d6340f84e3a4bd4d
JesseLiii <jesseli751@gmail.com>
2023-05-26 01:02:10 +0900

    fixed node_title

--- src/helpers/backend/updateNode.ts
+++ src/helpers/backend/updateNode.ts
@@ -3,101 +3,107 @@ import { GraphViewContextInterface } from '../../packages/graph/context/GraphVie
 export type NodeUpdate = 'resize' | 'drag' | 'title' | 'icon' | 'color';
 
 export const updateNode = (
-  type: NodeUpdate,
-  newVal: any,
-  nodeID: string,
-  context: GraphViewContextInterface | null
+	type: NodeUpdate,
+	newVal: any,
+	nodeID: string,
+	context: GraphViewContextInterface | null
 ) => {
-  console.log('new val ' + newVal.done);
-  let graphNodes = context?.nodeVisualData_Graph;
-  let nodeData = context?.nodeData_Graph;
-  if (!graphNodes || !nodeData) return;
+	console.log('new val ' + newVal.done);
+	let graphNodes = context?.nodeVisualData_Graph;
+	let nodeData = context?.nodeData_Graph;
+	if (!graphNodes || !nodeData) return;
 
-  switch (type) {
-    case 'drag':
-      const [x, y] = [newVal.x, newVal.y];
-      context?.addAction(nodeID, 'DRAG', {
-        new: {
-          x: x,
-          y: y,
-        },
-        old: {
-          x: graphNodes[nodeID].x,
-          y: graphNodes[nodeID].y,
-        },
-      });
-      graphNodes[nodeID].x = x;
-      graphNodes[nodeID].y = y;
+	switch (type) {
+		case 'drag':
+			const [x, y] = [newVal.x, newVal.y];
+			context?.addAction(nodeID, 'DRAG', {
+				new: {
+					x: x,
+					y: y,
+				},
+				old: {
+					x: graphNodes[nodeID].x,
+					y: graphNodes[nodeID].y,
+				},
+			});
+			graphNodes[nodeID].x = x;
+			graphNodes[nodeID].y = y;
 
-      context?.setnodeVisualData_Graph({ ...graphNodes });
+			context?.setnodeVisualData_Graph({ ...graphNodes });
 
-      break;
-    case 'resize':
-      if (!graphNodes[nodeID].y || !graphNodes[nodeID].x) return;
-      const newSize = [newVal.width, newVal.height];
-      const oldX = graphNodes[nodeID].x;
-      const oldY = graphNodes[nodeID].y;
-      let valX;
-      let valY;
-      if (newVal.tag === 'top') {
-        valY = graphNodes[nodeID].y + graphNodes[nodeID].height - newSize[1];
-        if (Number.isFinite(valY)) {
-          graphNodes[nodeID].y = valY;
-        }
-      }
-      if (newVal.tag === 'left') {
-        valX = graphNodes[nodeID].x + graphNodes[nodeID].width - newSize[0];
-        if (Number.isFinite(valX)) {
-          graphNodes[nodeID].x = valX;
-        }
-      }
-      if (newVal.done) {
-        context?.addAction(nodeID, 'NODE_SIZE', {
-          new: {
-            width: graphNodes[nodeID].width,
-            height: graphNodes[nodeID].height,
-            x: valX ? valX : oldX,
-            y: valY ? valY : oldY,
-          },
-          old: {
-            width: newVal.width,
-            height: newVal.height,
-            x: oldX,
-            y: oldY,
-          },
-        });
-        return;
-      }
+			break;
+		case 'resize':
+			if (!graphNodes[nodeID].y || !graphNodes[nodeID].x) return;
+			const newSize = [newVal.width, newVal.height];
+			const oldX = graphNodes[nodeID].x;
+			const oldY = graphNodes[nodeID].y;
+			let valX;
+			let valY;
+			if (newVal.tag === 'top') {
+				valY =
+					graphNodes[nodeID].y +
+					graphNodes[nodeID].height -
+					newSize[1];
+				if (Number.isFinite(valY)) {
+					graphNodes[nodeID].y = valY;
+				}
+			}
+			if (newVal.tag === 'left') {
+				valX =
+					graphNodes[nodeID].x +
+					graphNodes[nodeID].width -
+					newSize[0];
+				if (Number.isFinite(valX)) {
+					graphNodes[nodeID].x = valX;
+				}
+			}
+			if (newVal.done) {
+				context?.addAction(nodeID, 'NODE_SIZE', {
+					new: {
+						width: graphNodes[nodeID].width,
+						height: graphNodes[nodeID].height,
+						x: valX ? valX : oldX,
+						y: valY ? valY : oldY,
+					},
+					old: {
+						width: newVal.width,
+						height: newVal.height,
+						x: oldX,
+						y: oldY,
+					},
+				});
+				return;
+			}
 
-      graphNodes[nodeID].width = newSize[0];
-      graphNodes[nodeID].height = newSize[1];
-      context?.setnodeVisualData_Graph({ ...graphNodes });
-      break;
+			graphNodes[nodeID].width = newSize[0];
+			graphNodes[nodeID].height = newSize[1];
+			context?.setnodeVisualData_Graph({ ...graphNodes });
+			break;
 
-    case 'title':
-      context?.addAction(nodeID, 'NODE_TITLE', {
-        oldTitle: nodeData[nodeID].title,
-        title: newVal,
-      });
-      nodeData[nodeID].title = newVal;
+		case 'title':
+			context?.addAction(nodeID, 'NODE_TITLE', {
+				old: { title: nodeData[nodeID].title },
+				new: { title: newVal },
+			});
+			nodeData[nodeID].title = newVal;
 
-      context?.setnodeData_Graph({ ...nodeData });
-      break;
-    case 'icon':
-      context?.addAction(nodeID, 'NODE_ICON', {
-        new: { icon: newVal },
-        old: { icon: nodeData[nodeID].icon },
-      });
-      nodeData[nodeID].icon = newVal;
-      context?.setnodeData_Graph({ ...nodeData });
-      break;
-    case 'color':
-      context?.addAction(nodeID, 'NODE_COLOR', {
-        new: { color: newVal },
-        old: { color: nodeData[nodeID].color },
-      });
-      nodeData[nodeID].color = newVal;
-      context?.setnodeData_Graph({ ...nodeData });
-      break;
-  }
+			context?.setnodeData_Graph({ ...nodeData });
+			break;
+		case 'icon':
+			context?.addAction(nodeID, 'NODE_ICON', {
+				new: { icon: newVal },
+				old: { icon: nodeData[nodeID].icon },
+			});
+			nodeData[nodeID].icon = newVal;
+			context?.setnodeData_Graph({ ...nodeData });
+			break;
+		case 'color':
+			context?.addAction(nodeID, 'NODE_COLOR', {
+				new: { color: newVal },
+				old: { color: nodeData[nodeID].color },
+			});
+			nodeData[nodeID].color = newVal;
+			context?.setnodeData_Graph({ ...nodeData });
+			break;
+	}
 };

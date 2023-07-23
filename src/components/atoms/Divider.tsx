9e02a4af2dacfddb3ab02c637134dd2a52becac7
JesseLiii <jesseli751@gmail.com>
2023-07-23 13:21:22 +0200

    added logout button

--- src/components/atoms/Divider.tsx
+++ src/components/atoms/Divider.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Divider: React.FC<{ widthCSS: string }> = ({ widthCSS = 'w-full' }) => {
+const Divider: React.FC<{ widthCSS?: string }> = ({ widthCSS = 'w-full' }) => {
 	return <hr className={'border-t border-gray-300 my-4 h-0 ' + widthCSS} />;
 };
 
--- src/components/organisms/Settings.tsx
+++ src/components/organisms/Settings.tsx
@@ -1,19 +1,33 @@
+import { signOut, useSession } from 'next-auth/react';
 import React, { useEffect } from 'react';
+import TextButton from '../molecules/TextButton';
+import Divider from '../atoms/Divider';
 
 type SettingsProps = {};
 
 //
 const SettingsPanel: React.FC<SettingsProps> = () => {
-	useEffect(() => {
-		console.log(localStorage.getItem('userId'));
-		console.log(sessionStorage.getItem('userId'));
-		console.log(!sessionStorage.getItem('favourites'));
-	}, []);
+	const { data: session, status } = useSession();
 
+	if (status === 'authenticated') {
+		return (
+			<div>
+				<div>Hi, {session.user?.name}</div>
+				<div>
+					You're currently logged in as <b>{session.user?.email}</b>
+				</div>
+				<Divider />
+				<TextButton
+					text='Logout'
+					onClick={() => signOut()}
+				></TextButton>
+			</div>
+		);
+	} else {
+	}
 	return (
 		<div>
-			<div>Settings</div>
-			<div>okay more settings</div>
+			<div>Not logged in atm</div>
 		</div>
 	);
 };
